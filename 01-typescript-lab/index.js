1376461d2f6920659f3cb7369b6c42ceeeb4f0c8
markov98 <74935630+markov98@users.noreply.github.com>
2024-02-21 16:08:06 +0200

    playing around with classes

--- 01-typescript-lab/index.js
+++ 01-typescript-lab/index.js
@@ -1,2 +1,11 @@
-var a = 'Hello!';
-console.log(a);
+var Person = /** @class */ (function () {
+    function Person(name) {
+        this.name = name;
+    }
+    Person.prototype.sayHello = function () {
+        return "".concat(this.name, " says hello!");
+    };
+    return Person;
+}());
+var person = new Person('Nikolai');
+console.log(person.sayHello());
--- 01-typescript-lab/index.ts
+++ 01-typescript-lab/index.ts
@@ -1,2 +1,14 @@
-const a: String = 'Hello!';
-console.log(a);
+class Person {
+    private name: string;
+
+    constructor(name: string) {
+        this.name = name;
+    }
+
+    sayHello() {
+        return `${this.name} says hello!`
+    }
+}
+
+const person = new Person('Nikolai');
+console.log(person.sayHello());
