80ea214c861090d0f8c813ee0ea450a16289bca2
markov98 <74935630+markov98@users.noreply.github.com>
2024-03-03 21:36:53 +0200

    edited type to get themes method

--- 03-components-workshop/workshop/src/app/api.service.ts
+++ 03-components-workshop/workshop/src/app/api.service.ts
@@ -1,14 +1,18 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { Theme } from './types/theme';
+
+const { apiUrl } = environment;
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
-  constructor(private hhtp: HttpClient) { }
+  constructor(private http: HttpClient) { }
 
-  getThemes() {}
+  getThemes() { return this.http.get<Theme[]>(`${apiUrl}/themes`) }
 
   getPosts(limit?: number) { }
 }
