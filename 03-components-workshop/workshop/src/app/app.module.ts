518e8a1d1c1f884f918a0e6851085856ba01bf46
markov98 <74935630+markov98@users.noreply.github.com>
2024-03-10 10:23:10 +0200

    added theme-router, reorganized architecture

--- 03-components-workshop/workshop/src/app/app.module.ts
+++ 03-components-workshop/workshop/src/app/app.module.ts
@@ -6,18 +6,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { SharedModule } from './shared/shared.module';
-import { MainComponent } from './main/main.component';
-import { ThemesListComponent } from './themes-list/themes-list.component';
-import { PostsListComponent } from './posts-list/posts-list.component';
 import { HomeComponent } from './home/home.component';
 import { UserModule } from './user/user.module';
+import { ThemeModule } from './theme/theme.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainComponent,
-    ThemesListComponent,
-    PostsListComponent,
     HomeComponent
   ],
   imports: [
@@ -26,7 +21,8 @@ import { UserModule } from './user/user.module';
     CoreModule,
     SharedModule,
     HttpClientModule,
-    UserModule
+    UserModule,
+    ThemeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- 03-components-workshop/workshop/src/app/theme/theme.module.ts
+++ 03-components-workshop/workshop/src/app/theme/theme.module.ts
@@ -2,16 +2,26 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AddThemeComponent } from './add-theme/add-theme.component';
 import { CurrentThemeComponent } from './current-theme/current-theme.component';
+import { ThemeRoutingModule } from './theme-routing.module';
+import { MainComponent } from './main/main.component';
+import { ThemesListComponent } from './themes-list/themes-list.component';
+import { PostsListComponent } from './posts-list/posts-list.component';
+import { SharedModule } from '../shared/shared.module';
 
 
 
 @NgModule({
   declarations: [
     AddThemeComponent,
-    CurrentThemeComponent
+    CurrentThemeComponent,
+    MainComponent,
+    ThemesListComponent,
+    PostsListComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    ThemeRoutingModule,
+    SharedModule
   ]
 })
 export class ThemeModule { }
