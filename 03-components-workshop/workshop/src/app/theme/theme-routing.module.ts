9456c990e8b076df1351e849a79ca2fbadce1436
markov98 <74935630+markov98@users.noreply.github.com>
2024-03-10 11:33:57 +0200

    added new-theme route along with css and html for the coponent

--- 03-components-workshop/workshop/src/app/theme/theme-routing.module.ts
+++ 03-components-workshop/workshop/src/app/theme/theme-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MainComponent } from './main/main.component';
+import { AddThemeComponent } from './add-theme/add-theme.component';
 
 const routes: Routes = [
-    {path: 'home', component: MainComponent}
+    {path: 'home', component: MainComponent},
+    {path: 'new-theme', component: AddThemeComponent},
 ];
 
 @NgModule({
