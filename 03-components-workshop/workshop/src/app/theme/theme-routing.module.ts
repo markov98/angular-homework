c69ab902c71ae4c5350d16803511e7097da7b746
markov98 <74935630+markov98@users.noreply.github.com>
2024-03-11 07:50:41 +0200

    changed routing on thene module

--- 03-components-workshop/workshop/src/app/theme/theme-routing.module.ts
+++ 03-components-workshop/workshop/src/app/theme/theme-routing.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MainComponent } from './main/main.component';
 import { AddThemeComponent } from './add-theme/add-theme.component';
+import { CurrentThemeComponent } from './current-theme/current-theme.component';
 
 const routes: Routes = [
-    {path: 'home', component: MainComponent},
-    {path: 'new-theme', component: AddThemeComponent},
+  {
+    path: 'themes', children: [
+      { path: '', component: MainComponent },
+      { path: 'add', component: AddThemeComponent },
+      { path: ':themeId', component: CurrentThemeComponent }
+    ]
+  },
 ];
 
 @NgModule({
