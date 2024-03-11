c1258df9771a962ce203ef6e6139830b855f382b
markov98 <74935630+markov98@users.noreply.github.com>
2024-03-11 08:22:28 +0200

    added error component, along with css, html, and routing

--- 03-components-workshop/workshop/src/app/app-routing.module.ts
+++ 03-components-workshop/workshop/src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { ErrorComponent } from './error/error.component';
 
-const routes: Routes = [{ path: '', pathMatch: 'full', component: HomeComponent }];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', component: HomeComponent },
+  { path: '**', redirectTo: '404' },
+  { path: '404', pathMatch: 'full', component: ErrorComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
