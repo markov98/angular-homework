5dca53e0a274ab8de5df6a6793cfd44ce9493e83
markov98 <74935630+markov98@users.noreply.github.com>
2024-03-18 14:39:14 +0200

    added edit functionality to profile

--- 03-components-workshop/workshop/src/app/types/user.ts
+++ 03-components-workshop/workshop/src/app/types/user.ts
@@ -1,22 +1,27 @@
-import { Post } from "./post";
-import { Theme } from "./theme";
+import { Post } from './post';
 export interface User {
-    themes: Theme[];
-    posts: Post[];
-    _id: string;
-    tel: string;
-    email: string;
-    username: string;
-    password: string;
-    created_at: string;
-    updatedAt: string;
-    __v: number;
+  themes: string[];
+  posts: Post[];
+  _id: string;
+  tel: string;
+  email: string;
+  username: string;
+  password: string;
+  created_at: string;
+  updatedAt: string;
+  __v: number;
 }
 
 export interface UserForAuth {
-    firstName: string;
-    email: string;
-    phoneNumber: string;
-    password: string;
-    id: string;
-  }
+  firstName: string;
+  email: string;
+  phoneNumber: string;
+  password: string;
+  id: string;
+}
+
+export interface ProfileDetails {
+  username: string;
+  email: string;
+  tel: string;
+}
