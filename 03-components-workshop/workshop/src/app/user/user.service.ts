84e1dfa753dd5dbeaf9d6c6071a125eaee76540c
markov98 <74935630+markov98@users.noreply.github.com>
2024-03-24 23:08:39 +0200

    updated user service

--- 03-components-workshop/workshop/src/app/user/user.service.ts
+++ 03-components-workshop/workshop/src/app/user/user.service.ts
@@ -1,40 +1,77 @@
-import { Injectable } from '@angular/core';
+import { Injectable, OnDestroy } from '@angular/core';
 import { UserForAuth } from '../types/user';
+import { HttpClient } from '@angular/common/http';
+import { BehaviorSubject, Subscription, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
-export class UserService {
+export class UserService implements OnDestroy {
+  private user$$ = new BehaviorSubject<UserForAuth | undefined>(undefined);
+  private user$ = this.user$$.asObservable();
+
   user: UserForAuth | undefined;
   USER_KEY = '[user]';
+
+  userSubscription: Subscription;
 
   get isLogged(): boolean {
     return !!this.user;
   }
 
-  constructor() {
-    try {
-      const lsUser = localStorage.getItem(this.USER_KEY) || '';
-      this.user = JSON.parse(lsUser);
-    } catch (error) {
-      this.user = undefined;
-    }
+  constructor(private http: HttpClient) {
+    this.userSubscription = this.user$.subscribe((user) => {
+      this.user = user;
+    });
   }
 
-  login() {
-    this.user = {
-      id: '5fa64ca72183ce1728ff3726',
-      firstName: 'Petko',
-      email: 'petkoivanov@abv.bg',
-      password: '123123',
-      phoneNumber: '123-123-123-213',
-    };
+  login(email: string, password: string) {
+    return this.http
+      .post<UserForAuth>('/api/login', { email, password })
+      .pipe(tap((user) => this.user$$.next(user)));
+  }
 
-    localStorage.setItem(this.USER_KEY, JSON.stringify(this.user));
+  register(
+    username: string,
+    email: string,
+    tel: string,
+    password: string,
+    rePassword: string
+  ) {
+    return this.http
+      .post<UserForAuth>('/api/register', {
+        username,
+        email,
+        tel,
+        password,
+        rePassword,
+      })
+      .pipe(tap((user) => this.user$$.next(user)));
   }
 
   logout() {
-    this.user = undefined;
-    localStorage.removeItem(this.USER_KEY);
+    return this.http
+      .post('/api/logout', {})
+      .pipe(tap(() => this.user$$.next(undefined)));
+  }
+
+  getProfile() {
+    return this.http
+      .get<UserForAuth>('/api/users/profile')
+      .pipe(tap((user) => this.user$$.next(user)));
+  }
+
+  updateProfile(username: string, email: string, tel?: string) {
+    return this.http
+      .put<UserForAuth>('/api/users/profile', {
+        username,
+        email,
+        tel,
+      })
+      .pipe(tap((user) => this.user$$.next(user)));
+  }
+
+  ngOnDestroy(): void {
+    this.userSubscription.unsubscribe();
   }
 }
