925bd4d6d1a7e9413a6f40f61ef08bc80bb18abc
markov98 <74935630+markov98@users.noreply.github.com>
2024-03-26 19:44:33 +0200

    edited error component

--- 03-components-workshop/workshop/src/app/core/error/error.component.ts
+++ 03-components-workshop/workshop/src/app/core/error/error.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ErrorService } from './error.service';
 
 @Component({
   selector: 'app-error',
   templateUrl: './error.component.html',
-  styleUrls: ['./error.component.css']
+  styleUrls: ['./error.component.css'],
 })
-export class ErrorComponent {
+export class ErrorComponent implements OnInit {
+  errorMsg = '';
+  constructor(private errorService: ErrorService) {}
 
+  ngOnInit(): void {
+    this.errorService.apiError$.subscribe((err: any) => {
+      this.errorMsg = err?.message || '';
+    });
+  }
 }
