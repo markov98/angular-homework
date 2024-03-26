3e36cd030a87d395dd4bf7bb102de886df276567
markov98 <74935630+markov98@users.noreply.github.com>
2024-03-26 17:19:04 +0200

    added lazy loading

--- 03-components-workshop/workshop/src/app/app-routing.module.ts
+++ 03-components-workshop/workshop/src/app/app-routing.module.ts
@@ -6,6 +6,10 @@ import { ErrorComponent } from './core/error/error.component';
 const routes: Routes = [
   { path: '', pathMatch: 'full', component: HomeComponent },
   { path: '**', redirectTo: '/404' },
+  {
+    path: '',
+    loadChildren: () => import('./user/user.module').then((m) => m.UserModule),
+  },
   { path: '404', pathMatch: 'full', component: ErrorComponent }
 ];
 
--- 03-components-workshop/workshop/src/app/app.module.ts
+++ 03-components-workshop/workshop/src/app/app.module.ts
@@ -7,7 +7,6 @@ import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { SharedModule } from './shared/shared.module';
 import { HomeComponent } from './home/home.component';
-import { UserModule } from './user/user.module';
 import { ThemeModule } from './theme/theme.module';
 import { appInterceptorProvider } from './app.interceptor';
 
@@ -22,7 +21,6 @@ import { appInterceptorProvider } from './app.interceptor';
     CoreModule,
     SharedModule,
     HttpClientModule,
-    UserModule,
     ThemeModule,
     AppRoutingModule,
   ],
